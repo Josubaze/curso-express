2ab641a175a273a6ce4acb128c5a55b347d71517
Josue Baez <josuejbaeze4@gmail.com>
2023-12-21 16:29:48 -0400

    GET: recibir parámetros

--- index.js
+++ index.js
@@ -9,11 +9,39 @@ app.get('/', (req, res) => {
 app.get('/contact-us', (req, res) => {
   res.send('Contactanos via tlf: 0414589678 o email: @example.com');
 });
+
 app.get('/products', (req, res) => {
-  res.json({
+  res.json([
+  {
     name: 'producto 1',
     price: 500,
-  })
+  },
+  {
+    name: 'producto 2',
+    price: 600,
+  },
+  {
+    name: 'producto 3',
+    price: 450,
+  },
+  ])
+});
+
+app.get('/products/:id',(req, res)=>{
+  const { id } = req.params;
+  res.json({
+    id: id,
+    name: 'Product 2',
+    price: 600,
+  });
+})
+
+app.get('/categories/:categoriesId/products/:productId', (req, res)=>{
+  const { categoriesId, productId } = req.params;
+  res.json({
+    categoriesId,
+    productId,
+  });
 });
 
 app.listen(port, () => {
