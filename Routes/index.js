b821ce3563ec2c242fe5bd5daf78ceb3be62e511
Josue Baez <josuejbaeze4@gmail.com>
2023-12-27 17:13:47 -0400

    POST: método para crear + Insomia

--- Routes/index.js
+++ Routes/index.js
@@ -1,13 +1,18 @@
+const express = require('express');
+
 const productsRouter = require('./productsRouter');
 const usersRouter = require('./usersRouter');
 const contactusRouter = require('./contactusRouter');
 const categoriesRouter = require('./categoriesRouter');
 
 function routerApi(app){
-  app.use('/api/products', productsRouter);
-  app.use('/api/users', usersRouter);
-  app.use('/api/contact-us', contactusRouter);
-  app.use('/api/categories', categoriesRouter);
+  const router = express.Router();
+  app.use('/api/v1', router);
+
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/contact-us', contactusRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 module.exports = routerApi;
--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const routerApi = require('./Routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // para recibir data tipo json
+
 app.get('/', (req, res) => {
   res.send('Hola mi server en Express');
 });
+
 
 routerApi(app);
 
